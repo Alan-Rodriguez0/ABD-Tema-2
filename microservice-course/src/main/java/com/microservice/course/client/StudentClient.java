278f0f0f1269a47e2990d51a13a3314fd8d51ff8
Alan-Rodriguez0 <alan.rodsilv0@gmail.com>
2025-12-13 23:40:55 -0600

    Unidad 2

--- microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
+++ microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-student", url = "localhost:8080/api/student" )
+@FeignClient(name = "msvc-student")  // Solo nombre de Eureka
 public interface StudentClient {
-
-    @GetMapping("/search-by-course/{idCourse}")
+    @GetMapping("/api/student/search-by-course/{idCourse}")
     List<StudentDTO> findAllStudentByCourse(@PathVariable Long idCourse);
 }
+
