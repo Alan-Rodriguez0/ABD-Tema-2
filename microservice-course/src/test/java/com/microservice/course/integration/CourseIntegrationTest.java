81bf23a50adcb55247b9c4ee7916aaf99d46742d
Alan-Rodriguez0 <alan.rodsilv0@gmail.com>
2026-05-31 10:00:45 -0600

    Unidad 2

--- microservice-course/src/test/java/com/microservice/course/integration/CourseIntegrationTest.java
+++ microservice-course/src/test/java/com/microservice/course/integration/CourseIntegrationTest.java
@@ -13,7 +13,6 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
