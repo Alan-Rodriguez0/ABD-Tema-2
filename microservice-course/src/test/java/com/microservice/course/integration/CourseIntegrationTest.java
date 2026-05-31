fa4ad9a2151d8f8b5f342341449504cc0d56a49a
Alan-Rodriguez0 <alan.rodsilv0@gmail.com>
2026-05-31 10:00:10 -0600

    Unidad 2

--- microservice-course/src/test/java/com/microservice/course/integration/CourseIntegrationTest.java
+++ microservice-course/src/test/java/com/microservice/course/integration/CourseIntegrationTest.java
@@ -9,10 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -33,7 +34,7 @@ class CourseIntegrationTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private StudentClient studentClient;
 
     @BeforeEach
